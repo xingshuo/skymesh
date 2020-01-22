9ee7de8d81e6665eebf4c1d2c6bf0acf950edaa4
lakefu(傅强) <lakefu@tencent.com>
2020-01-22 21:48:38 +0800

    init

---
+++ examples/helloworld/greeter_client/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/xingshuo/skymesh/log"
+)
+
+func main() {
+	fmt.Print("greet begin!!\n")
+	log.Infof("greeting...\n")
+	fmt.Print("greet end!!\n")
+}
---
+++ examples/helloworld/greeter_server/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/xingshuo/skymesh/log"
+)
+
+func main() {
+	fmt.Print("server begin!!\n")
+	log.Infof("recv greeting...\n")
+	fmt.Print("greet quit!!\n")
+}
