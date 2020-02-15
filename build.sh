c482ec822406e19a17bec960de3961ee76f9f6a2
lakefu <lakefu@tencent.com>
2020-02-15 19:29:49 +0800

    编译模式改为:-mod=vendor go. generate by: go mod vendor -v

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo "build proto done."
 
 cd nameserver/bootstrap
 rm nameserver.exe
-go build -gcflags "-N -l" -o nameserver.exe main.go
+go build -gcflags "-N -l" -mod=vendor -o nameserver.exe main.go
 cd ../../
 cp nameserver/bootstrap/nameserver.exe examples/helloworld/nameserver/main.exe
 cp nameserver/bootstrap/nameserver.exe examples/nameservice/nameserver/main.exe
@@ -12,24 +12,24 @@ echo "build nameserver done."
 
 cd examples/helloworld/greeter_client
 rm main.exe
-go build -gcflags "-N -l" -o main.exe main.go
+go build -gcflags "-N -l" -mod=vendor -o main.exe main.go
 cd ../../../
 echo "build greeter client done."
 
 cd examples/helloworld/greeter_server
 rm main.exe
-go build -gcflags "-N -l" -o main.exe main.go
+go build -gcflags "-N -l" -mod=vendor -o main.exe main.go
 cd ../../../
 echo "build greeter server done."
 
 cd examples/nameservice/client
 rm main.exe
-go build -gcflags "-N -l" -o main.exe main.go
+go build -gcflags "-N -l" -mod=vendor -o main.exe main.go
 cd ../../../
 echo "build nameservice client done"
 
 cd examples/nameservice/server
 rm main.exe
-go build -gcflags "-N -l" -o main.exe main.go
+go build -gcflags "-N -l" -mod=vendor -o main.exe main.go
 cd ../../../
 echo "build nameservice server done"
